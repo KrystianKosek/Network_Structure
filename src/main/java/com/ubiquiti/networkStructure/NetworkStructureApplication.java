1cb981f0667737f2c6177a60a56a17e900f3f587
KrystianKosek <krystian.kosek17@gmail.com>
2024-06-26 20:38:39 +0200

    Initial commit

---
+++ src/main/java/com/ubiquiti/networkStructure/NetworkStructureApplication.java
@@ -0,0 +1,13 @@
+package com.ubiquiti.networkStructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkStructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubiquiti/networkStructure/NetworkStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubiquiti.networkStructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
